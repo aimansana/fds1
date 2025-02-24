1cb416cd39a54666240cce19bc70dfbf8948ebb9
aiman <139196530+aimansana@users.noreply.github.com>
2025-02-24 22:53:44 +0530

    Update officer1.php

--- officer1.php
+++ officer1.php
@@ -1,3 +1,33 @@
 <?php
-echo "off1 is here";
+echo "off5 is here";
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Subsidy Payment Officer Dashboard</title>
+    <link rel="stylesheet" href="off.css">
+</head>
+<body>
+    <header>
+        <h1>Subsidy Payment Officer Dashboard</h1>
+    </header>
+
+    <main>
+        <!-- Subsidy Payment Officer Dashboard -->
+        <section id="subsidy-payment-dashboard" class="dashboard">
+           
+            <button onclick="viewPendingPayments()">View Pending Payments</button>
+            <button onclick="viewFarmerDetails()">View Farmer Details</button>
+            <button onclick="viewPreviousPayments()">View Previous Completed Payments</button>
+        </section>
+    </main>
+
+    <!-- Linking subsidypayment.js -->
+    <script src="off5.js"></script>
+</body>
+</html>
+
+
